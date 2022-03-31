a235b414a9c49194485a971049e81ead19144960
kkbha <kkbha@Kamal>
2022-03-31 18:15:08 +0530

    Commit

---
+++ Day4_Package/src/com/java1/Example1.java
@@ -0,0 +1,8 @@
+package com.java1;
+
+public class Example1 {
+public void Link() {
+	System.out.println("Welcome to Java");
+	}
+
+}
---
+++ Day4_Package/src/com/java2/Example2.java
@@ -0,0 +1,13 @@
+package com.java2;
+import com.java1.*;
+
+public class Example2 {
+	
+
+	public static void main(String[] args) {
+		Example1 e=new Example1();
+		e.Link();
+
+	}
+
+}
