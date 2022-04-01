c2bd4571a1b0d350154a3db2e72d8afc641d3b27
kkbha <kkbha@Kamal>
2022-04-01 18:25:09 +0530

    First Commit

---
+++ Day5_Inheritence/src/SingleInheritence.java
@@ -0,0 +1,20 @@
+ class Superclass {
+	void print() {
+		System.out.println("Hi my name is Kamal");
+	}
+ }
+
+class Subclass extends Superclass{
+	void display() {
+		System.out.println("Inheritence");
+	}
+}
+	
+class SingleInheritence{
+
+public static void main(String[] args) {
+		Subclass s=new Subclass();
+		s.print();
+		s.display();
+}
+	}
