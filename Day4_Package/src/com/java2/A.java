1f6a9c43fa63507b34fa9bba5ebc4537afbfc097
kkbha <kkbha@Kamal>
2022-04-01 11:48:18 +0530

    Commit

---
+++ Day4_Package/src/com/java2/A.java
@@ -0,0 +1,9 @@
+package com.java2;
+
+public class A {
+
+	public void display() {
+		System.out.println("Core Java");
+	}
+
+}
---
+++ Day4_Package/src/com/java4/B.java
@@ -0,0 +1,13 @@
+package com.java4;
+import com.java2.*;
+
+public class B {
+
+	public static void main(String[] args) {
+		A obj=new A();
+		obj.display();
+		
+
+	}
+
+}
