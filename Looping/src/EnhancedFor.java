78e18b5238ed7df8c43d2e21b868485289817c6d
kkbha <kkbha@Kamal>
2022-04-03 19:46:24 +0530

    Commit

---
+++ Looping/src/EnhancedFor.java
@@ -0,0 +1,12 @@
+
+public class EnhancedFor {
+
+	public static void main(String[] args) {
+		String array[]= {"Riya","Saurav","Shubham","Jacky","Pinky"};
+		for(String x:array) {
+			System.out.println(x);
+		}
+
+	}
+
+}
