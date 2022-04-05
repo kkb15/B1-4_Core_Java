e05fd175fa4a41f79507ac7d8b7b654f143e5021
kkb15 <102526499+kkb15@users.noreply.github.com>
2022-04-06 01:05:20 +0530

    Commit

---
+++ Assignment1/src/Do_While.java
@@ -0,0 +1,19 @@
+	import java.util.Scanner;
+	public class Do_While {
+
+		public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int i=1;
+		do {
+			System.out.println(i);
+			i++;
+		}
+		while(i<=n);
+			sc.close();
+		}
+				
+
+		}
+
+	
